1354afb4bae1cfa4b99b2f5d1a0b4716efc61e64
adam-tam <50637239+adam-tam@users.noreply.github.com>
2020-09-11 15:13:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var x = 0;
 var slides = [];
 var images = [];
 
-slides[0] = 
+slides[0] = '';
 slides[1] = 'exp1inv.mp4';
 slides[2] = 'exp2inv.mp4';
 slides[3] = 'exp3inv.mp4';
 slides[4] = 'exp4inv.mp4';
 
-images[0] = 
+images[0] = '';
 images[1] = 'img1.png';
 images[2] = 'img2.png';
 images[3] = 'img3.png';
